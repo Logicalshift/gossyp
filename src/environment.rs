42820c23bfb39c685a49acc2a023748f2cae83d7
Andrew Hunter <andrew@logicalshift.co.uk>
2017-03-01 22:01:30 +0000

    Add an initial implementation of the environment class

--- src/environment.rs
+++ src/environment.rs
@@ -1,3 +1,15 @@
-trait Environment {
-    
+use tool::*;
+
+///
+/// Represents a tooling environment, which enables tools to be retrieved by name.
+///
+/// Environments make it possible to use tools without needing to know where the
+/// precise implementation is located. Environments also provide a way to perform
+/// dependency injection in other tools.
+///
+pub trait Environment {
+    ///
+    /// Retrieves a tool using a JSON interface by name
+    ///
+    fn get_json_tool<'a>(&self, name: &str) -> Result<&'a JsonTool, Error>;
 }
