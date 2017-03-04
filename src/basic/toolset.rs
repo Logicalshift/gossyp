8171c33c938b80456f490d51459a3cf621ec195d
Andrew Hunter <andrew@logicalshift.co.uk>
2017-03-04 20:48:00 +0000

    Add a basic toolset we can use to generate tools from vectors
    Would quite like to just have vectors act as basic toolsets but rust isn’t smart enough to do that yet

--- src/basic/toolset.rs
+++ src/basic/toolset.rs
@@ -26,7 +26,48 @@ pub trait ToolSet {
     ///
     /// Creates the tools in this toolset
     ///
-    fn create_tools(&self, environment: &Environment) -> Vec<Box<NamedTool>>;
+    fn create_tools(self, environment: &Environment) -> Vec<Box<NamedTool>>;
+}
+
+///
+/// Represents a simple toolset that just returns a constant set of tools
+///
+pub struct BasicToolSet {
+    tools: Vec<Box<NamedTool>>
+}
+
+impl BasicToolSet {
+    pub fn from<T: NamedTool+'static>(source: Vec<T>) -> BasicToolSet {
+        let mut result: Vec<Box<NamedTool>> = vec![];
+
+        for item in source {
+            result.push(Box::new(item));
+        }
+
+        BasicToolSet { tools: result }
+    }
+}
+
+impl ToolSet for BasicToolSet {
+    fn create_tools(self, _environment: &Environment) -> Vec<Box<NamedTool>> {
+        self.tools
+    }
+}
+
+impl<'a, T: Tool> Tool for (&'a str, T) {
+    #[inline]
+    fn invoke_json(&self, input: Value, environment: &Environment) -> Result<Value, Value> {
+        let (_, ref tool) = *self;
+        tool.invoke_json(input, environment)
+    }
+}
+
+impl<'a, T: Tool> NamedTool for (&'a str, T) {
+    #[inline]
+    fn get_name<'b>(&'b self) -> &'b str {
+        let (ref name, _) = *self;
+        *name
+    }
 }
 
 impl<T: Tool> Tool for (String, T) {
@@ -47,9 +88,6 @@ impl<T: Tool> NamedTool for (String, T) {
 
 #[cfg(test)]
 mod test {
-    use std::result::Result;
-    use serde_json::*;
-
     use super::*;
     use super::super::functional_tool::*;
     use super::super::empty_environment::*;
@@ -57,7 +95,7 @@ mod test {
     #[test]
     fn can_invoke_named_tool() {
         let empty       = EmptyEnvironment::new();
-        let base_tool   = make_tool(|x: i32| -> Result<i32, ()> { Ok(x+1) });
+        let base_tool   = make_pure_tool(|x: i32| { x+1 });
         let named_tool  = (String::from("name"), base_tool);
 
         let result = named_tool.invoke_json(json![ 2 ], &empty);
@@ -66,8 +104,27 @@ mod test {
 
     #[test]
     fn can_get_tool_name() {
-        let base_tool   = make_tool(|x: i32| -> Result<i32, ()> { Ok(x+1) });
+        let base_tool   = make_pure_tool(|x: i32| { x+1 });
         let named_tool  = (String::from("name"), base_tool);
+
+        let name = named_tool.get_name();
+        assert!(name == "name");
+    }
+
+    #[test]
+    fn can_invoke_named_tool_str() {
+        let empty       = EmptyEnvironment::new();
+        let base_tool   = make_pure_tool(|x: i32| { x+1 });
+        let named_tool  = ("name", base_tool);
+
+        let result = named_tool.invoke_json(json![ 2 ], &empty);
+        assert!(result == Ok(json![ 3 ]));
+    }
+
+    #[test]
+    fn can_get_tool_name_str() {
+        let base_tool   = make_pure_tool(|x: i32| { x+1 });
+        let named_tool  = ("name", base_tool);
 
         let name = named_tool.get_name();
         assert!(name == "name");
