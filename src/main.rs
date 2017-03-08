5e00971cd6ce7815fbba75207d60f78622e9effb
Andrew Hunter <andrew@logicalshift.co.uk>
2017-03-08 21:30:34 +0000

    Print out a header using the I/O tools

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
+extern crate silkthread_base;
+extern crate silkthread_toolkit;
 
+use silkthread_base::basic::*;
+use silkthread_toolkit::io::*;
+
+fn main() {
+    // Start up
+    let main_env = DynamicEnvironment::new();
+    main_env.import(IoTools::new_stdio());
+
+    // Display header
+    let print_string = main_env.get_typed_tool::<String, ()>("print").unwrap();
+    print_string.invoke(format!("{} {} by {}\n", env!("CARGO_PKG_NAME"),  env!("CARGO_PKG_VERSION"), env!("CARGO_PKG_AUTHORS")), &main_env).unwrap();
 }
