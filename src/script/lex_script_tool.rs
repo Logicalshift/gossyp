966bf159c612692d059602153177d68676f29398
Andrew Hunter <andrew@logicalshift.co.uk>
2017-03-12 20:05:50 +0000

    Add some tests to make sure we can parse the syntax JSON file

--- src/script/lex_script_tool.rs
+++ src/script/lex_script_tool.rs
@@ -18,3 +18,37 @@ pub fn create_lex_script_tool() -> StringLexingTool {
     // Create the lexing tool with this definition
     StringLexingTool::from_lex_tool_input(&lex_defn)
 }
+
+#[cfg(test)]
+mod test {
+    use std::error::Error;
+    use super::*;
+
+    #[test]
+    fn can_parse_syntax_json() {
+        let script_json = from_str::<Value>(include_str!("syntax_lexer.json"));
+
+        if script_json.is_err() {
+            println!("{:?}", script_json);
+            println!("{:?}", script_json.unwrap_err().description());
+
+            assert!(false);
+        }
+    }
+
+    #[test]
+    fn json_can_be_deserialized() {
+        let script_json = from_str::<Vec<LexToolSymbol>>(include_str!("syntax_lexer.json"));
+
+        if script_json.is_err() {
+            println!("{:?}", script_json);
+        }
+
+        script_json.unwrap();
+    }
+
+    #[test]
+    fn can_create_tool() {
+        let _tool = create_lex_script_tool();
+    }
+}
