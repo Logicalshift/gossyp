da7ef2b58736a794fb3d0d783fc4ccd6f1737211
Andrew Hunter <andrew@logicalshift.co.uk>
2017-03-12 19:58:08 +0000

    Rename to ScriptTools for consistency with the toolkit library

--- src/script/mod.rs
+++ src/script/mod.rs
@@ -8,10 +8,10 @@ use silkthread_base::basic::*;
 ///
 /// ToolSet for dealing with the scripting language
 ///
-pub struct ScriptToolSet {
+pub struct ScriptTools {
 }
 
-impl ToolSet for ScriptToolSet {
+impl ToolSet for ScriptTools{
     fn create_tools(self, _: &Environment) -> Vec<(String, Box<Tool>)> {
         vec![
             (String::from(tool::LEX_SCRIPT), Box::new(create_lex_script_tool()))
@@ -19,11 +19,11 @@ impl ToolSet for ScriptToolSet {
     }
 }
 
-impl ScriptToolSet {
+impl ScriptTools {
     ///
     /// Creates a new script toolset
     ///
-    pub fn new() -> ScriptToolSet {
-        ScriptToolSet { }
+    pub fn new() -> ScriptTools {
+        ScriptTools { }
     }
 }
