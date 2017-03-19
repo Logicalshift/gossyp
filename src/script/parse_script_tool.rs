662a6700f30be283fa97287b7e248b617c0f937c
Andrew Hunter <andrew@logicalshift.co.uk>
2017-03-19 10:30:47 +0000

    Going to try restructuring this a bit

--- src/script/parse_script_tool.rs
+++ src/script/parse_script_tool.rs
@@ -21,125 +21,155 @@ impl ParseError {
     }
 }
 
-///
-/// Returns true if a token is considered syntax (gets returned from lookahead)
-///
-fn is_syntax(token: &ScriptToken) -> bool {
-    match token.token {
-        ScriptLexerToken::Whitespace    |
-        ScriptLexerToken::Comment       => false,
-
-        _                               => true
-    }
+struct ParseState<'a> {
+    remaining: &'a [ScriptToken]
 }
 
-///
-/// Looks ahead to the next syntactically relevant lexer match (and returns the tokens after it)
-///
-fn lookahead<'a>(input: &'a [ScriptToken]) -> Option<(&'a ScriptToken, &'a [ScriptToken])> {
-    let mut index   = 0;
-    let len         = input.len();
+impl<'a> ParseState<'a> {
+    ///
+    /// Returns true if a token is considered syntax (gets returned from lookahead)
+    ///
+    fn is_syntax(token: &ScriptToken) -> bool {
+        match token.token {
+            ScriptLexerToken::Whitespace    |
+            ScriptLexerToken::Comment       => false,
 
-    loop {
-        if index >= len {
-            return None;
-        } else if is_syntax(&input[index]) {
-            return Some((&input[index], &input[index+1..len]));
+            _                               => true
         }
-
-        index += 1;
     }
-}
 
-///
-/// Parses a statement
-///
-pub fn parse_statement<'a>(input: &'a [ScriptToken]) -> Result<(Script, &'a [ScriptToken]), ParseError> {
-    if let Some((lookahead, remainder)) = lookahead(input) {
-        match lookahead.token {
-            // Newlines are ignored
-            ScriptLexerToken::Newline       => parse_statement(remainder),
+    ///
+    /// Looks ahead to the next syntactically relevant lexer match (and returns the tokens after it)
+    ///
+    fn lookahead(&self) -> Option<(&'a ScriptToken, &'a [ScriptToken])> {
+        let mut index   = 0;
+        let len         = self.remaining.len();
 
-            ScriptLexerToken::Let           => parse_let(remainder),
-            ScriptLexerToken::Var           => parse_var(remainder),
-            ScriptLexerToken::Def           => parse_def(remainder),
-            ScriptLexerToken::If            => parse_if(remainder),
-            ScriptLexerToken::Using         => parse_using(remainder),
-            ScriptLexerToken::While         => parse_while(remainder),
-            ScriptLexerToken::Loop          => parse_loop(remainder),
-            ScriptLexerToken::For           => parse_for(remainder),
+        loop {
+            if index >= len {
+                return None;
+            } else if ParseState::is_syntax(&self.remaining[index]) {
+                let token       = &self.remaining[index];
+                let remaining   = &self.remaining[index+1..len];
+                return Some((token, remaining));
+            }
 
-            ScriptLexerToken::Identifier    => parse_command(input),
-
-            // Unrecognised token
-            _ => Err(ParseError::new())
+            index += 1;
         }
-    } else {
-        // EOF
-        Err(ParseError::new())
     }
-}
 
-///
-/// Parses a command
-///
-/// Syntax '<expression>', '<expression> <expression>'
-///
-pub fn parse_command<'a>(input: &'a [ScriptToken]) -> Result<(Script, &'a [ScriptToken]), ParseError> {
-    unimplemented!();
-}
-
-///
-/// Parses an Expression
-///
-pub fn parse_expression<'a>(input: &'a [ScriptToken]) -> Result<(Expression, &'a [ScriptToken]), ParseError> {
-    if let Some((lookahead, remainder)) = lookahead(input) {
-        let expr1 = match lookahead.token {
-            ScriptLexerToken::Identifier    => Ok((Expression::Identifier(lookahead.clone()), remainder)),
-            ScriptLexerToken::Number        |
-            ScriptLexerToken::HexNumber     => Ok((Expression::Number(lookahead.clone()), remainder)),
-            ScriptLexerToken::String        => Ok((Expression::String(lookahead.clone()), remainder)),
-
-            _ => Err(ParseError::new())
-        };
-
-        expr1
-    } else {
-        // EOF
-        Err(ParseError::new())
+    ///
+    /// If the next token matches the specified token, consumes it and returns
+    /// its content.
+    ///
+    fn accept(&mut self, token: ScriptLexerToken) -> Option<&'a ScriptToken> {
+        if let Some((lookahead, remaining)) = self.lookahead() {
+            // Found a token
+            if lookahead.token == token {
+                self.remaining = remaining;
+                Some(lookahead)
+            } else {
+                None
+            }
+        } else {
+            // Reached the end of file
+            None
+        }
     }
-}
 
-pub fn parse_let<'a>(input: &'a [ScriptToken]) -> Result<(Script, &'a [ScriptToken]), ParseError> {
-    unimplemented!()
-}
+    /*
 
-pub fn parse_var<'a>(input: &'a [ScriptToken]) -> Result<(Script, &'a [ScriptToken]), ParseError> {
-    unimplemented!()
-}
+    ///
+    /// Parses a statement
+    ///
+    pub fn parse_statement<'a>(input: &'a [ScriptToken]) -> Result<(Script, &'a [ScriptToken]), ParseError> {
+        if let Some((lookahead, remainder)) = lookahead(input) {
+            match lookahead.token {
+                // Newlines are ignored
+                ScriptLexerToken::Newline       => parse_statement(remainder),
 
-pub fn parse_def<'a>(input: &'a [ScriptToken]) -> Result<(Script, &'a [ScriptToken]), ParseError> {
-    unimplemented!()
-}
+                ScriptLexerToken::Let           => parse_let(remainder),
+                ScriptLexerToken::Var           => parse_var(remainder),
+                ScriptLexerToken::Def           => parse_def(remainder),
+                ScriptLexerToken::If            => parse_if(remainder),
+                ScriptLexerToken::Using         => parse_using(remainder),
+                ScriptLexerToken::While         => parse_while(remainder),
+                ScriptLexerToken::Loop          => parse_loop(remainder),
+                ScriptLexerToken::For           => parse_for(remainder),
 
-pub fn parse_if<'a>(input: &'a [ScriptToken]) -> Result<(Script, &'a [ScriptToken]), ParseError> {
-    unimplemented!()
-}
+                ScriptLexerToken::Identifier    => parse_command(input),
 
-pub fn parse_using<'a>(input: &'a [ScriptToken]) -> Result<(Script, &'a [ScriptToken]), ParseError> {
-    unimplemented!()
-}
+                // Unrecognised token
+                _ => Err(ParseError::new())
+            }
+        } else {
+            // EOF
+            Err(ParseError::new())
+        }
+    }
 
-pub fn parse_while<'a>(input: &'a [ScriptToken]) -> Result<(Script, &'a [ScriptToken]), ParseError> {
-    unimplemented!()
-}
+    ///
+    /// Parses a command
+    ///
+    /// Syntax '<expression>', '<expression> <expression>'
+    ///
+    pub fn parse_command<'a>(input: &'a [ScriptToken]) -> Result<(Script, &'a [ScriptToken]), ParseError> {
+        unimplemented!();
+    }
 
-pub fn parse_loop<'a>(input: &'a [ScriptToken]) -> Result<(Script, &'a [ScriptToken]), ParseError> {
-    unimplemented!()
-}
+    ///
+    /// Parses an Expression
+    ///
+    pub fn parse_expression<'a>(input: &'a [ScriptToken]) -> Result<(Expression, &'a [ScriptToken]), ParseError> {
+        if let Some((lookahead1, remainder1)) = lookahead(input) {
+            let expr1 = match lookahead1.token {
+                ScriptLexerToken::Identifier    => Ok((Expression::Identifier(lookahead1.clone()), remainder)),
+                ScriptLexerToken::Number        |
+                ScriptLexerToken::HexNumber     => Ok((Expression::Number(lookahead1.clone()), remainder)),
+                ScriptLexerToken::String        => Ok((Expression::String(lookahead1.clone()), remainder)),
 
-pub fn parse_for<'a>(input: &'a [ScriptToken]) -> Result<(Script, &'a [ScriptToken]), ParseError> {
-    unimplemented!()
+                _ => Err(ParseError::new())
+            };
+
+            expr1
+        } else {
+            // EOF
+            Err(ParseError::new())
+        }
+    }
+
+    pub fn parse_let<'a>(input: &'a [ScriptToken]) -> Result<(Script, &'a [ScriptToken]), ParseError> {
+        unimplemented!()
+    }
+
+    pub fn parse_var<'a>(input: &'a [ScriptToken]) -> Result<(Script, &'a [ScriptToken]), ParseError> {
+        unimplemented!()
+    }
+
+    pub fn parse_def<'a>(input: &'a [ScriptToken]) -> Result<(Script, &'a [ScriptToken]), ParseError> {
+        unimplemented!()
+    }
+
+    pub fn parse_if<'a>(input: &'a [ScriptToken]) -> Result<(Script, &'a [ScriptToken]), ParseError> {
+        unimplemented!()
+    }
+
+    pub fn parse_using<'a>(input: &'a [ScriptToken]) -> Result<(Script, &'a [ScriptToken]), ParseError> {
+        unimplemented!()
+    }
+
+    pub fn parse_while<'a>(input: &'a [ScriptToken]) -> Result<(Script, &'a [ScriptToken]), ParseError> {
+        unimplemented!()
+    }
+
+    pub fn parse_loop<'a>(input: &'a [ScriptToken]) -> Result<(Script, &'a [ScriptToken]), ParseError> {
+        unimplemented!()
+    }
+
+    pub fn parse_for<'a>(input: &'a [ScriptToken]) -> Result<(Script, &'a [ScriptToken]), ParseError> {
+        unimplemented!()
+    }
+    */
 }
 
 impl ParseScriptTool {
