ad23706e4346a00d56f102a9480c1ea3da9db7a5
Andrew Hunter <andrew@logicalshift.co.uk>
2017-04-05 21:13:11 +0100

    Add bindings for simple identifiers

--- src/script/bind_expression.rs
+++ src/script/bind_expression.rs
@@ -1,3 +1,4 @@
+use std::rc::*;
 use std::result::Result;
 use serde_json::*;
 
@@ -51,12 +52,35 @@ fn parse_number(number: &str) -> Value {
 }
 
 ///
+/// Creates an execution error relating to an expression
+///
+fn generate_expression_error(error: ScriptEvaluationError, expr: &Expression) -> Value {
+    json![{
+        "error":                error,
+        "failed-expression":    expr
+    }]
+}
+
+///
+/// Generates a tool binding
+///
+pub fn bind_tool(tool_name: &ScriptToken, expr: &Expression, execution_environment: &ScriptExecutionEnvironment) -> Result<BoundExpression, Value> {
+    if let Ok(tool) = execution_environment.get_json_tool(&tool_name.matched) {
+        Ok(BoundExpression::Tool(Rc::new(tool), tool_name.clone()))
+    } else {
+        Err(generate_expression_error(ScriptEvaluationError::ExpressionDoesNotEvaluateToTool, expr))
+    }
+}
+
+///
 /// Binds an expression to an environment
 ///
-pub fn bind_expression(expr: &Expression, execution_environment: &mut ScriptExecutionEnvironment) -> Result<BoundExpression, Value> {
+pub fn bind_expression(expr: &Expression, execution_environment: &ScriptExecutionEnvironment) -> Result<BoundExpression, Value> {
     match expr {
         &Expression::String(ref s)          => Ok(BoundExpression::Value(Value::String(unquote_string(&s.matched)), s.clone())),
         &Expression::Number(ref n)          => Ok(BoundExpression::Value(parse_number(&n.matched), n.clone())),
+
+        &Expression::Identifier(ref id)     => bind_tool(id, expr, execution_environment),
 
         _ => unimplemented!()
     }
@@ -83,5 +107,18 @@ mod test {
         let mut env             = ScriptExecutionEnvironment::new(&empty_environment);
 
         assert!(match bind_expression(&string_expr, &mut env) { Ok(BoundExpression::Value(num, _)) => num == json![ 42 ], _ => false }); 
+    }
+
+    #[test]
+    fn can_bind_tool() {
+        let tool_expr           = Expression::identifier("test");
+        let tool_environment    = DynamicEnvironment::new();
+
+        tool_environment.define("test", Box::new(make_pure_tool(|_: ()| "Success")));
+
+        let mut env             = ScriptExecutionEnvironment::new(&tool_environment);
+        let result              = bind_expression(&tool_expr, &mut env);
+
+        assert!(match result { Ok(BoundExpression::Tool(_, _)) => true, _ => false });
     }
 }
