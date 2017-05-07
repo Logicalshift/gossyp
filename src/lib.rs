46c8c138f23d23af236011f2bf220b0f9ee12baa
Andrew Hunter <andrew@logicalshift.co.uk>
2017-05-07 11:29:33 +0100

    Use static initialization for the lexing tool
    This speeds up tests and other places we might want to repeatedly reinitialise that particular tool

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 extern crate serde;
 #[macro_use] extern crate serde_json;
 #[macro_use] extern crate serde_derive;
+#[macro_use] extern crate lazy_static;
 extern crate concordance;
 extern crate gossyp_base;
 
