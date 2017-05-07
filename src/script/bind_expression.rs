d12a5ac503ca4bfe4889b0b79f7fbe828992f9fe
Andrew Hunter <andrew@logicalshift.co.uk>
2017-05-07 11:12:23 +0100

    Add array and map binding

--- src/script/bind_expression.rs
+++ src/script/bind_expression.rs
@@ -73,16 +73,97 @@ pub fn bind_tool(tool_name: &ScriptToken, expr: &Expression, execution_environme
 }
 
 ///
+/// Binds a sequence of elements
+///
+fn bind_sequence(items: &Vec<Expression>, execution_environment: &ScriptExecutionEnvironment) -> Result<Vec<BoundExpression>, Value> {
+    let mut result = vec![];
+
+    for expr in items {
+        match bind_expression(expr, execution_environment) {
+            Ok(val)     => result.push(val),
+            Err(erm)    => return Err(erm)
+        }
+    }
+
+    Ok(result)
+}
+
+///
+/// Generates an array binding
+///
+pub fn bind_array(items: &Vec<Expression>, execution_environment: &ScriptExecutionEnvironment) -> Result<BoundExpression, Value> {
+    bind_sequence(items, execution_environment)
+        .map(|array_items| BoundExpression::Array(array_items))
+}
+
+///
+/// Generates a tuple binding
+///
+pub fn bind_tuple(items: &Vec<Expression>, execution_environment: &ScriptExecutionEnvironment) -> Result<BoundExpression, Value> {
+    bind_sequence(items, execution_environment)
+        .map(|tuple_items| BoundExpression::Tuple(tuple_items))
+}
+
+///
+/// Generates a map binding
+///
+pub fn bind_map(items: &Vec<(Expression, Expression)>, execution_environment: &ScriptExecutionEnvironment) -> Result<BoundExpression, Value> {
+    let mut result = vec![];
+
+    for &(ref lexpr, ref rexpr) in items {
+        let lbound = match bind_expression(lexpr, execution_environment) {
+            Ok(val)     => val,
+            Err(erm)    => return Err(erm)
+        };
+
+        let rbound = match bind_expression(rexpr, execution_environment) {
+            Ok(val)     => val,
+            Err(erm)    => return Err(erm)
+        };
+
+        result.push((lbound, rbound));
+    }
+
+    Ok(BoundExpression::Map(result))
+}
+
+///
+/// Binds an index expression (a[b])
+///
+pub fn bind_index(index: &Box<(Expression, Expression)>, execution_environment: &ScriptExecutionEnvironment) -> Result<BoundExpression, Value> {
+    unimplemented!();
+}
+
+///
+/// Binds a field access expression (a.b)
+///
+pub fn bind_field_access(field_access: &Box<(Expression, Expression)>, execution_environment: &ScriptExecutionEnvironment) -> Result<BoundExpression, Value> {
+    unimplemented!();
+}
+
+///
+/// Binds an apply expression (a(parameters))
+///
+pub fn bind_apply(apply: &Box<(Expression, Expression)>, execution_environment: &ScriptExecutionEnvironment) -> Result<BoundExpression, Value> {
+    unimplemented!();
+}
+
+///
 /// Binds an expression to an environment
 ///
 pub fn bind_expression(expr: &Expression, execution_environment: &ScriptExecutionEnvironment) -> Result<BoundExpression, Value> {
     match expr {
-        &Expression::String(ref s)          => Ok(BoundExpression::Value(Value::String(unquote_string(&s.matched)), s.clone())),
-        &Expression::Number(ref n)          => Ok(BoundExpression::Value(parse_number(&n.matched), n.clone())),
+        &Expression::String(ref s)              => Ok(BoundExpression::Value(Value::String(unquote_string(&s.matched)), s.clone())),
+        &Expression::Number(ref n)              => Ok(BoundExpression::Value(parse_number(&n.matched), n.clone())),
 
-        &Expression::Identifier(ref id)     => bind_tool(id, expr, execution_environment),
+        &Expression::Array(ref items)           => bind_array(items, execution_environment),
+        &Expression::Tuple(ref items)           => bind_tuple(items, execution_environment),
+        &Expression::Map(ref items)             => bind_map(items, execution_environment),
 
-        _ => unimplemented!()
+        &Expression::Identifier(ref id)         => bind_tool(id, expr, execution_environment),
+        &Expression::Index(ref indexer)         => bind_index(indexer, execution_environment),
+        &Expression::FieldAccess(ref accessor)  => bind_field_access(accessor, execution_environment),
+        &Expression::Apply(ref application)     => bind_apply(application, execution_environment),
     }
 }
 
@@ -120,5 +201,55 @@ mod test {
         let result              = bind_expression(&tool_expr, &mut env);
 
         assert!(match result { Ok(BoundExpression::Tool(_, _)) => true, _ => false });
+    }
+
+    #[test]
+    fn can_bind_array() {
+        let array_expr          = Expression::Array(vec![Expression::identifier("test"), Expression::number("1")]);
+        let tool_environment    = DynamicEnvironment::new();
+
+        tool_environment.define("test", Box::new(make_pure_tool(|_: ()| "Success")));
+
+        let mut env             = ScriptExecutionEnvironment::new(&tool_environment);
+        let result              = bind_expression(&array_expr, &mut env);
+
+        assert!(match result { Ok(BoundExpression::Array(_)) => true, _ => false });
+
+        let array = match result { Ok(BoundExpression::Array(x)) => x, _ => vec![] };
+        assert!(match array[0] { BoundExpression::Tool(_, _) => true, _ => false });
+        assert!(match array[1] { BoundExpression::Value(_, _) => true, _ => false });
+    }
+
+    #[test]
+    fn can_bind_tuple() {
+        let tuple_expr          = Expression::Tuple(vec![Expression::identifier("test"), Expression::number("1")]);
+        let tool_environment    = DynamicEnvironment::new();
+
+        tool_environment.define("test", Box::new(make_pure_tool(|_: ()| "Success")));
+
+        let mut env             = ScriptExecutionEnvironment::new(&tool_environment);
+        let result              = bind_expression(&tuple_expr, &mut env);
+
+        assert!(match result { Ok(BoundExpression::Tuple(_)) => true, _ => false });
+
+        let tuple = match result { Ok(BoundExpression::Tuple(x)) => x, _ => vec![] };
+        assert!(match tuple[0] { BoundExpression::Tool(_, _) => true, _ => false });
+        assert!(match tuple[1] { BoundExpression::Value(_, _) => true, _ => false });
+    }
+
+    #[test]
+    fn can_bind_map() {
+        let map_expr            = Expression::Map(vec![(Expression::string("\"test\""), Expression::identifier("test"))]);
+        let tool_environment    = DynamicEnvironment::new();
+
+        tool_environment.define("test", Box::new(make_pure_tool(|_: ()| "Success")));
+
+        let mut env             = ScriptExecutionEnvironment::new(&tool_environment);
+        let result              = bind_expression(&map_expr, &mut env);
+
+        assert!(match result { Ok(BoundExpression::Map(_)) => true, _ => false });
+
+        let map = match result { Ok(BoundExpression::Map(x)) => x, _ => vec![] };
+        assert!(match map[0] { (BoundExpression::Value(_, _), BoundExpression::Tool(_, _)) => true, _ => false });
     }
 }
