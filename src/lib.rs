b3f377ef762b4161834eda12772a3feea734c11c
Andrew Hunter <andrew@logicalshift.co.uk>
2017-05-27 15:30:07 +0100

    Not actually sure if this will work but we might be able to re-export the various libraries we build so gossyp can be a library too

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+extern crate serde_json;
+extern crate gossyp_base;
+extern crate gossyp_toolkit;
+extern crate gossyp_lang;
+
+pub use gossyp_base::*;
+pub use gossyp_toolkit::*;
+pub use gossyp_lang::*;
