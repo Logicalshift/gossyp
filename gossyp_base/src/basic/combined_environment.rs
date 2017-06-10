200a527f4b59c0e6df5cd2868e322b30e925d740
Andrew Hunter <andrew@logicalshift.co.uk>
2017-06-10 15:38:36 +0100

    Change CombinedEnvironment to combine environment references instead of boxed environments

--- gossyp_base/src/basic/combined_environment.rs
+++ gossyp_base/src/basic/combined_environment.rs
@@ -14,22 +14,22 @@ use super::list_tools::*;
 use super::empty_environment::*;
 use super::functional_tool::*;
 
-struct EnvironmentCollection {
-    environments: Vec<Box<Environment>>
+struct EnvironmentCollection<'a> {
+    environments: Vec<&'a Environment>
 }
 
-impl EnvironmentCollection {
-    fn new(environments: Vec<Box<Environment>>) -> EnvironmentCollection {
+impl<'a> EnvironmentCollection<'a> {
+    fn new(environments: Vec<&'a Environment>) -> EnvironmentCollection<'a> {
         EnvironmentCollection { environments: environments }
     }
 }
 
 #[derive(Clone)]
-pub struct CombinedEnvironment {
-    collection: Arc<Mutex<EnvironmentCollection>>
+pub struct CombinedEnvironment<'a> {
+    collection: Arc<Mutex<EnvironmentCollection<'a>>>
 }
 
-impl CombinedEnvironment {
+impl<'a> CombinedEnvironment<'a> {
     ///
     /// Creates a new combined environment
     ///
@@ -37,7 +37,7 @@ impl CombinedEnvironment {
     /// one environment defines one. The `list-tools` tool will be changed to one that combines the
     /// results across all environments.
     ///
-    pub fn from_environments(environments: Vec<Box<Environment>>) -> CombinedEnvironment {
+    pub fn from_environments(environments: Vec<&'a Environment>) -> CombinedEnvironment {
         CombinedEnvironment { collection: Arc::new(Mutex::new(EnvironmentCollection::new(environments))) }
     }
 
@@ -48,11 +48,19 @@ impl CombinedEnvironment {
         let collection = self.collection.lock().unwrap();
 
         // List all of the tools in all the environments
-        let tools = collection.environments.iter()
+        let tools: Vec<Box<Tool>> = collection.environments.iter()
             .map(|env| env.get_json_tool(super::tool_name::LIST_TOOLS))
             .filter(|tool| tool.is_ok())
-            .map(|tool| tool.unwrap());
+            .map(|tool| tool.unwrap())
+            .collect();
         
+        CombinedEnvironment::collect_tool_list(tools.iter()) 
+    }
+
+    ///
+    /// Given a collection of list-tools implementations, returns the result of combining them all
+    ///
+    fn collect_tool_list<'b, T: Iterator<Item=&'b Box<Tool>>>(tools: T) -> ListToolsResult {
         let results = tools.map(|tool| tool.invoke_json(Value::Null, &EmptyEnvironment::new()))
             .filter(|result| result.is_ok())
             .map(|result| result.unwrap());
@@ -74,13 +82,19 @@ impl CombinedEnvironment {
     }
 }
 
-impl Environment for CombinedEnvironment {
+impl<'a> Environment for CombinedEnvironment<'a> {
     fn get_json_tool(&self, name: &str) -> Result<Box<Tool>, RetrieveToolError> {
         if name == super::tool_name::LIST_TOOLS {
-            // Generate a list-tools implementation (we use a copy of this object, which will reference
-            // our environments as the collection is reference counted)
-            let list_environment    = self.clone();
-            let list_tools          = make_pure_tool(move |_: ()| list_environment.list_tools());
+            // Get all the list-tools implementations from our environments
+            let collection              = self.collection.lock().unwrap();
+            let tools: Vec<Box<Tool>>   = collection.environments.iter()
+                .map(|env| env.get_json_tool(super::tool_name::LIST_TOOLS))
+                .filter(|tool| tool.is_ok())
+                .map(|tool| tool.unwrap())
+                .collect();
+
+            // Create a combined list-tools implementation
+            let list_tools = make_pure_tool(move |_: ()| CombinedEnvironment::collect_tool_list(tools.iter()));
 
             Ok(Box::new(list_tools))
         } else {
@@ -109,7 +123,7 @@ mod test {
         first.define("first-tool", Box::new(make_pure_tool(|x:i32| x+1)));
         second.define("second-tool", Box::new(make_pure_tool(|x:i32| x+2)));
 
-        let combined = CombinedEnvironment::from_environments(vec![ Box::new(first), Box::new(second) ]);
+        let combined = CombinedEnvironment::from_environments(vec![ &first, &second ]);
 
         assert!(combined.get_json_tool("first-tool").is_ok());
         assert!(combined.get_typed_tool("first-tool").unwrap().invoke(2, &combined) == Ok(3));
@@ -123,7 +137,7 @@ mod test {
         first.define("first-tool", Box::new(make_pure_tool(|x:i32| x+1)));
         second.define("second-tool", Box::new(make_pure_tool(|x:i32| x+2)));
 
-        let combined = CombinedEnvironment::from_environments(vec![ Box::new(first), Box::new(second) ]);
+        let combined = CombinedEnvironment::from_environments(vec![ &first, &second ]);
 
         assert!(combined.get_json_tool("second-tool").is_ok());
         assert!(combined.get_typed_tool("second-tool").unwrap().invoke(2, &combined) == Ok(4));
@@ -137,7 +151,7 @@ mod test {
         first.define("tool", Box::new(make_pure_tool(|x:i32| x+1)));
         second.define("tool", Box::new(make_pure_tool(|x:i32| x+2)));
 
-        let combined = CombinedEnvironment::from_environments(vec![ Box::new(first), Box::new(second) ]);
+        let combined = CombinedEnvironment::from_environments(vec![ &first, &second ]);
 
         assert!(combined.get_json_tool("tool").is_ok());
         assert!(combined.get_typed_tool("tool").unwrap().invoke(2, &combined) == Ok(3));
@@ -151,7 +165,7 @@ mod test {
         first.define("first-tool", Box::new(make_pure_tool(|x:i32| x+1)));
         second.define("second-tool", Box::new(make_pure_tool(|x:i32| x+2)));
 
-        let combined = CombinedEnvironment::from_environments(vec![ Box::new(first), Box::new(second) ]);
+        let combined = CombinedEnvironment::from_environments(vec![ &first, &second ]);
 
         assert!(combined.get_json_tool("list-tools").is_ok());
         assert!(combined.get_typed_tool("list-tools").unwrap().invoke((), &combined) == Ok(ListToolsResult::with_names(vec![ "define-tool", "first-tool", "list-tools", "second-tool", "undefine-tool" ])));
@@ -165,7 +179,7 @@ mod test {
         first.define("tool", Box::new(make_pure_tool(|x:i32| x+1)));
         second.define("tool", Box::new(make_pure_tool(|x:i32| x+2)));
 
-        let combined = CombinedEnvironment::from_environments(vec![ Box::new(first), Box::new(second) ]);
+        let combined = CombinedEnvironment::from_environments(vec![ &first, &second ]);
 
         assert!(combined.get_json_tool("list-tools").is_ok());
         assert!(combined.get_typed_tool("list-tools").unwrap().invoke((), &combined) == Ok(ListToolsResult::with_names(vec![ "define-tool", "list-tools", "tool", "undefine-tool" ])));
