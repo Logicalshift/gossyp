7a8dcf8e5ba63a1418b18eb91173bb7b677cca3c
Andrew Hunter <andrew@logicalshift.co.uk>
2017-06-10 15:38:17 +0100

    Make Environments Send+Sync
    This is needed to be able to have environments that reference environments

--- gossyp_base/src/environment.rs
+++ gossyp_base/src/environment.rs
@@ -7,7 +7,7 @@ use tool::*;
 /// precise implementation is located. Environments also provide a way to perform
 /// dependency injection in other tools.
 ///
-pub trait Environment : Send {
+pub trait Environment : Send+Sync {
     ///
     /// Retrieves a tool using a JSON interface by name
     ///
